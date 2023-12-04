ba383315f996c5bf7735f898064f5904c947db3f
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2023-12-04 11:00:04 +0700

    Modify main file and remove unused api file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(feature = "cli")]
+use footballscore::{config::Config, football_opts::FootballOpts, Error};
+
+#[cfg(not(tarpaulin_include))]
+#[cfg(feature = "cli")]
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let config = Config::init_config(None)?;
+
+    match tokio::spawn(async move { FootballOpts::parse_opts(&config).await })
+        .await
+        .unwrap()
+    {
+        Ok(()) => Ok(()),
+        Err(Error::InvalidInputError(e)) => {
+            let help_message = FootballOpts::api_help_msg();
+            println!("{e}\n{help_message}");
+
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
+}
+
+#[cfg(not(tarpaulin_include))]
+#[cfg(not(feature = "cli"))]
+fn main() -> Result<(), Error> {
+    Ok(())
 }
