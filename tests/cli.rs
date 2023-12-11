82dd8d7f1d9b959c8c904063b98e3865c451ad8e
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2023-12-11 14:19:48 +0700

    Modify default CLI test

--- tests/cli.rs
+++ tests/cli.rs
@@ -7,20 +7,51 @@ use footballscore::config::TestEnvs;
 #[ignore]
 #[test]
 fn test_default() -> Result<(), Error> {
-    let _env = TestEnvs::new(&["API_KEY", "API_ENDPOINT", "CLUB_ID", "API_PATH"]);
+    let _env = TestEnvs::new(&["API_KEY", "API_ENDPOINT", "CLUB_ID"]);
     let bin = cargo_bin("footballscore");
     assert!(bin.exists());
 
-    let output = Command::cargo_bin("footballscore")?
+    let output_live_fixture = Command::cargo_bin("footballscore")?
         .args(["-k", "1e5765fc0c22df4e4ccf20581c2ef3d7", "-c", "529"])
         .output()?;
 
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    let stderr = String::from_utf8_lossy(&output.stderr);
+    let stdout = String::from_utf8_lossy(&output_live_fixture.stdout);
+    let stderr = String::from_utf8_lossy(&output_live_fixture.stderr);
     info!("{}", stdout);
     info!("{}", stderr);
 
-    assert!(stdout.contains("Match: no live event"));
+    assert!(
+        stdout.contains("Error: token - Error/Missing application key. Go to https://www.api-football.com/documentation-v3 to learn how to get your API application key.")
+        || stdout.contains("Match: no live event")
+    );
+
+    let output_next_fixture = Command::cargo_bin("footballscore")?
+        .args(["-k", "1e5765fc0c22df4e4ccf20581c2ef3d7", "-c", "529", "--next-match", "1"])
+        .output()?;
+
+    let stdout = String::from_utf8_lossy(&output_next_fixture.stdout);
+    let stderr = String::from_utf8_lossy(&output_next_fixture.stderr);
+    info!("{}", stdout);
+    info!("{}", stderr);
+
+    assert!(
+        stdout.contains("Error: token - Error/Missing application key. Go to https://www.api-football.com/documentation-v3 to learn how to get your API application key.")
+        || stdout.contains("Barcelona")
+    );
+
+    let output_team_information = Command::cargo_bin("footballscore")?
+        .args(["-k", "1e5765fc0c22df4e4ccf20581c2ef3d7", "-n", "arsenal"])
+        .output()?;
+
+    let stdout = String::from_utf8_lossy(&output_team_information.stdout);
+    let stderr = String::from_utf8_lossy(&output_team_information.stderr);
+    info!("{}", stdout);
+    info!("{}", stderr);
+
+    assert!(
+        stdout.contains("Error: token - Error/Missing application key. Go to https://www.api-football.com/documentation-v3 to learn how to get your API application key.")
+        || stdout.contains("Here's your club information:")
+    );
 
     Ok(())
 }
