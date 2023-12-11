cdb1137b39a29ab337e47b036beab88ddeb6e850
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2023-12-11 14:20:42 +0700

    Update benchmark file to handle teams data

--- benches/footballscore_benchmark.rs
+++ benches/footballscore_benchmark.rs
@@ -1,13 +1,22 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use footballscore::football_data::FootballData;
+use footballscore::{
+    football_fixtures_data::FootballFixturesData, football_teams_data::FootballTeamsData,
+};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let buf = include_str!("../tests/resource/fixtures.json");
-    let data: FootballData = serde_json::from_str(buf).unwrap();
+    let data: FootballFixturesData = serde_json::from_str(buf).unwrap();
 
-    c.bench_function("footballscore_data", |b| {
+    c.bench_function("footballscore_fixtures_data", |b| {
         b.iter(|| data.get_current_fixtures())
+    });
+
+    let buf = include_str!("../tests/resource/teams.json");
+    let data: FootballTeamsData = serde_json::from_str(buf).unwrap();
+
+    c.bench_function("footballscore_teams_data", |b| {
+        b.iter(|| data.get_teams_information())
     });
 }
 
